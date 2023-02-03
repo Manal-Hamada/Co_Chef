8a4dc7816da618cf6c8a800b6ab3f66f03a303dd
Co_Chef <manalhamada1999@gmail.com>
2023-02-03 10:57:13 +0200

    First push

---
+++ app/src/main/java/com/example/foodplanner_app/View/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.foodplanner_app.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.foodplanner_app.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
