3d61154889d4c1fb6e3d988223cee2b0669dfb8d
Co_Chef <manalhamada1999@gmail.com>
2023-02-07 14:42:29 +0200

    First milestone virsion

--- app/src/main/java/com/example/foodplanner_app/view/HomeActivity.java
+++ app/src/main/java/com/example/foodplanner_app/view/HomeActivity.java
@@ -1,15 +1,19 @@
 package com.example.foodplanner_app.view;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager.widget.ViewPager;
 
+import android.app.FragmentManager;
 import android.os.Bundle;
 
 import com.example.foodplanner_app.Model;
 import com.example.foodplanner_app.R;
+import com.example.foodplanner_app.databinding.ActivityHomeBinding;
 import com.google.android.material.badge.BadgeDrawable;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.tabs.TabLayout;
@@ -24,13 +28,14 @@ public class HomeActivity extends AppCompatActivity {
     Home_Fragment homeFragment;
     DailyPlanFragment menuFragment ;
     FavouritFragment favouritFragmentn;
-
+    ActivityHomeBinding binding;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        binding=ActivityHomeBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
         init();
         setTabLayout();
         //setFragmentTrasaction();
@@ -39,19 +44,35 @@ public class HomeActivity extends AppCompatActivity {
     }
 
     public void init(){
-        Home_Fragment homeFragment = new Home_Fragment();
+      /*  Home_Fragment homeFragment = new Home_Fragment();
         DailyPlanFragment menuFragment = new DailyPlanFragment();
-        FavouritFragment favouritFragmentn = new FavouritFragment();
+        FavouritFragment favouritFragmentn = new FavouritFragment();*/
         tab=findViewById(R.id.tablayout);
-        bottomNavigationView=findViewById(R.id.bottomNavigationView);
+       // bottomNavigationView=findViewById(R.id.bottomNavigationView);
+        binding.bottomNavigationView.setOnItemReselectedListener(item -> {
+            switch (item.getItemId()){
+
+                case R.id.fav:
+                     replaceFragments(new Home_Fragment());
+                    break;
+                case R.id.menu:
+                    replaceFragments(new DailyPlanFragment());
+                    break;
+                case R.id.profile:
+                    replaceFragments(new DailyPlanFragment());
+                    break;
+                case  R.id.home:
+                    replaceFragments(new Home_Fragment());
+                    break;
+
+            }
+        });
 
     }
-    public void setFragmentTrasaction(){
-        getSupportFragmentManager().beginTransaction().replace(R.id.container,menuFragment).commit();
 
-        BadgeDrawable badgeDrawable = bottomNavigationView.getOrCreateBadge(R.id.home);
-        badgeDrawable.setVisible(true);
-        badgeDrawable.setNumber(8);
+    public void replaceFragments(Fragment fragment){
+        getSupportFragmentManager().beginTransaction().replace(R.id.nav_tab_lay_out,fragment).commit();
+
     }
     public void setTabLayout(){
         ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
