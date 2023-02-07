3192878dde2c6c0716781dbbbe6efa517afa9efd
hossamfadaly99 <hossamfdali1@gmail.com>
2023-02-07 17:46:01 +0200

    commit to pull

---
+++ app/src/main/java/com/example/foodplanner_app/OnBoardingItem.java
@@ -0,0 +1,41 @@
+package com.example.foodplanner_app;
+
+public class OnBoardingItem {
+    private int image;
+    private String title;
+    private String description;
+
+    public OnBoardingItem(){
+
+    }
+
+    public OnBoardingItem(int image, String title, String description) {
+        this.image = image;
+        this.title = title;
+        this.description = description;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
